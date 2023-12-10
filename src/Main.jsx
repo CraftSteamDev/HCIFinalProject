b8af0d49b1cbfd8cb7231303b95c058a847b3e38
Harry Rubin <hnrubin@wpi.edu>
2023-12-10 14:07:16 -0500

    changes

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,10 +7,14 @@ import './styles/Main.css'
 const Main = (props) => {
     const nav = useNavigate()
     return (
-        <div className={"box"}>
+        <div className={"mainbox"}>
             <h1 className={"title"}>Choose One</h1>
-            <Button className={"button"} onClick={() => nav("/createTimer")} variant={"contained"}>Timer</Button>
-            <Button className={"button"} onClick={() => nav("/exercise")} variant={"contained"}>Exercise</Button>
+            <div className={"buttonbox"}>
+                <Button className={"button"} onClick={() => nav("/createTimer")} variant={"contained"}>Timer</Button>
+            </div>
+            <div className={"buttonbox"}>
+                <Button className={"button"} onClick={() => nav("/exercise")} variant={"contained"}>Exercise</Button>
+            </div>
         </div>
     )
 
